4ea41bce3494270a696c827a4a33cba6b70296e3
Philippe-dev <philippe@dissitou.org>
2021-06-08 14:06:11 +0200

    Template files update

--- js/grayscale.js
+++ js/grayscale.js
@@ -13,24 +13,6 @@ $(window).scroll(function () {
   }
 });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function () {
-  $('a.page-scroll').bind('click', function (event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
-  $('.simple-menu a').bind('click', function (event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
-});
-
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').on('click', function () {
   $('.navbar-toggle:visible').click();
