7f5cd6a4f93525aa2b0633e604f4d2cd0c9cad71
Philippe-dev <philippe@dissitou.org>
2021-06-19 23:40:04 +0200

    Update popup_media.js

--- js/popup_media.js
+++ js/popup_media.js
@@ -13,14 +13,14 @@ $(function () {
         const thumburl = $('input[name="src"]').eq(2).val();
         const buttonId = main.$('input[name="change-button-id"]').val();
 
-        if (buttonId == 'default-image-selector') {
-            main.$('#default-image-url').prop('value', href).trigger('change');
+        if (buttonId == 'default-image-selector') {   
             main.$('#default-image-tb-url').prop('value', thumburl);
+            main.$('#default-image-url').prop('value', href).trigger('change');
         } else {
             for (let i = 0; i < 6; i++) {
                 if (buttonId == 'random-image-' + i + '-selector') {
-                    main.$('#random-image-' + i + '-url').prop('value', href).trigger('change');
                     main.$('#random-image-' + i + '-tb-url').prop('value', thumburl);
+                    main.$('#random-image-' + i + '-url').prop('value', href).trigger('change');
                 }
             }
         }
