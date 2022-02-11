70d3b9292e578cf13c215c1ba8d5acd79dc87c25
Philippe-dev <philippe@dissitou.org>
2022-02-11 10:57:44 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -16,7 +16,7 @@ $this->registerModule(
     'Grayscale',                           				// Name
     'Grayscale Bootstrap 5 theme for Dotclear',  		// Description
     'Philippe aka amalgame and contributors',           // Author
-    '2.5.1',                                       		// Version
+    '2.5.2',                                       		// Version
     [                                          			// Properties
         'requires'          => [['core', '2.19']], 		// Dependencies
         'standalone_config' => true,
