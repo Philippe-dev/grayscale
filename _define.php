c0686d7697c1eeb1180a535bbb0f44dacf1c106b
“philippe <philippe.mail.perso@gmail.com>
2025-09-08 10:33:02 +0200

    new version

--- _define.php
+++ _define.php
@@ -16,9 +16,10 @@ $this->registerModule(
     'Grayscale',
     'Grayscale Bootstrap 5 theme for Dotclear',
     'Philippe aka amalgame and contributors',
-    '4.4',
+    '4.5',
     [
-        'requires'          => [['core', '2.28']],
+        'date'        => '2025-09-08T00:00:08+0100',
+        'requires'    => [['core', '2.36']],
         'standalone_config' => true,
         'type'              => 'theme',
         'tplset'            => 'mustek',
